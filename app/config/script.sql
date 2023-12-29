30d5f29c1f22408975b5d0f4683f697acabb23ce
Mehdi <elmahdi20erraji@gmail.com>
2023-12-29 12:10:05 +0100

    modifying the sql script

--- app/config/script.sql
+++ app/config/script.sql
@@ -1,61 +1,118 @@
-create database immoConnect;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 29, 2023 at 11:04 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
-create table roles(
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-    Id int PRIMARY KEY ,
-    name varchar(100)
-);
 
-create table users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255),
-    email VARCHAR(255),
-    PASSWORD VARCHAR(255),
-    image VARCHAR(255),
-    statut int,
-    role_id int,
-    FOREIGN KEY (role_id) REFERENCES role(id)
-    
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table properties(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    adress VARCHAR(255),
-    surface FLOAT,
-    room int,
-    shower int,
-    price INT, 
-    statut INT,
-    type VARCHAR(255),
-    description text,
-    user_id int,
-    FOREIGN KEY (user_id) REFERENCES user(id)
-);
-create table comment (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    CommentContent VARCHAR(255),
-    SendTime date ,
-    user_id int,
-    properties_id int,
-    FOREIGN KEY (role_id) REFERENCES user(id),
-    FOREIGN KEY (properties_id ) REFERENCES properties(id)
-    
-);
+--
+-- Database: `immoconnect`
+--
 
-create table message-user(
-    id int AUTO_INCREMENT,
-    message text,
-    sendTime date,
-    senderId int,
-    recieverId int,
-    FOREIGN key (senderId) REFERENCES user(id),
-    FOREIGN key (recieverId) REFERENCES user(id)
+-- --------------------------------------------------------
 
-);
+--
+-- Table structure for table `comment`
+--
 
-CREATE TABLE images (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    imageUrl VARCHAR(255),
-    property_id INT,
-    FOREIGN KEY (property_id) REFERENCES properties(id)
-);
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `CommentContent` varchar(255) DEFAULT NULL,
+  `SendTime` date DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  `properties_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `properties_id` (`properties_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message_user`
+--
+
+DROP TABLE IF EXISTS `message_user`;
+CREATE TABLE IF NOT EXISTS `message_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `message` text,
+  `sendTime` date DEFAULT NULL,
+  `sender_Id` int DEFAULT NULL,
+  `reciever_Id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sender_Id` (`sender_Id`),
+  KEY `reciever_Id` (`reciever_Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properties`
+--
+
+DROP TABLE IF EXISTS `properties`;
+CREATE TABLE IF NOT EXISTS `properties` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `adress` varchar(255) DEFAULT NULL,
+  `surface` float DEFAULT NULL,
+  `room` int DEFAULT NULL,
+  `shower` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `statut` int DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `description` text,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `Id` int NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `PASSWORD` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `statut` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
