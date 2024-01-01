204c071a6064a16c463054ceb94d2543b93a8e7b
Rassili <adnan.rassili47@gmail.com>
2024-01-01 15:55:21 +0100

    add router

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\controllers;
+class HomeController{
+    public function index(){
+        include __DIR__ .'/../../index.php';
+    }
+}
---
+++ app/routes/RouteHandler.php
@@ -0,0 +1,45 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+use App\routes\Router;
+
+$router = new Router();
+
+$router->setRoutes([
+    'GET'=>[
+        ''=>['HomeController' , 'index'],
+        'home'=>['HomeController' , 'index'],
+
+
+    ],
+    'POST'=>[]
+]);
+if (isset($_GET['url'])) {
+    $uri = trim($_GET['url'], '/');
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    try {
+        $route = $router->getRoute($method, $uri);
+        if ($route) {
+            list($controllerName, $methodName) = $route;
+            $controllerClass = 'App\\controllers\\' . ucfirst($controllerName);
+            $object = new $controllerClass();
+
+            if ($methodName) {
+                if (method_exists($object, $methodName)) {
+                    $object->$methodName();
+                } else {
+                    throw new Exception('Method not found in controller.');
+                }
+            } else {
+                $object->index();
+            }
+        } else {
+            throw new Exception('Route not found.');
+        }
+    } catch (Exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n";
+        
+    }
+} else {
+    echo 'error';
+}
